1811084098d9c1793f650ed20647da01038bcf9a
Arben Bakiu <arben.bakiu91@gmail.com>
2019-05-04 18:34:47 +0200

    change listening for event change method signature of neural network constructor reorganize example code

--- src/irisclassification/IrisClassification.java
+++ src/irisclassification/IrisClassification.java
@@ -9,6 +9,7 @@ import org.neuroph.contrib.transfer.ExponentialLinearUnit;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.events.LearningEvent;
 import org.neuroph.core.events.LearningEventListener;
+import org.neuroph.core.transfer.Sigmoid;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.BackPropagation;
 import org.neuroph.util.data.norm.Normalizer;
@@ -20,10 +21,15 @@ import org.neuroph.util.data.norm.RangeNormalizer;
  */
 public class IrisClassification {
 
+    static int MAX_ITERATIONS = 20000;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        double bpLearningRate = 0.03d;
+        
         int inputNum=4;
         int outputNum=3;
         String dataSetFile = "Iris-dataset-normalized.txt";
@@ -34,34 +40,60 @@ public class IrisClassification {
         irisData.shuffle();
         DataSet[] trainTest = irisData.createTrainingAndTestSubsets(60, 40);
         
-        MultiLayerPerceptron neuralNetELU = new MultilayerPerceptronELU(new ExponentialLinearUnit(0.5), inputNum, 16, outputNum);
+        
+        MultiLayerPerceptron neuralNetELU = new MultilayerPerceptronELU(new ExponentialLinearUnit(0.1), inputNum, 16, outputNum);
         BackPropagation bpELU = neuralNetELU.getLearningRule();
-        bpELU.setLearningRate(0.6);
+        bpELU.setLearningRate(bpLearningRate);
+        bpELU.setMaxIterations(MAX_ITERATIONS);
+        bpELU.addListener(getListener("ELU"));
+        neuralNetELU.learn(irisData); 
         
-        bpELU.addListener(new LearningEventListener() {
-            @Override
-            public void handleLearningEvent(LearningEvent event) {
-                BackPropagation bp = (BackPropagation) event.getSource();
-                System.out.println("Iteration: "+bp.getCurrentIteration() + " Error: "+bp.getTotalNetworkError());
-            }
-        });
         
+        //example of regular neural net with Sigmoid transfer
         MultiLayerPerceptron neuralNet = new MultiLayerPerceptron(inputNum, 16, outputNum);
         BackPropagation bp = neuralNet.getLearningRule();
+//        bp.setLearningRate(bpLearningRate);
         bp.setLearningRate(0.6);
+        bp.setMaxIterations(MAX_ITERATIONS);
+        bp.addListener(getListener("Sigmoid"));
+        neuralNet.learn(irisData);
         
-        bp.addListener(new LearningEventListener() {
+        
+
+        neuralNetELU = new MultilayerPerceptronELU(new ExponentialLinearUnit(0.1), inputNum, 16, 14, outputNum);
+        bpELU = neuralNetELU.getLearningRule();
+        bpELU.setLearningRate(bpLearningRate);
+        bpELU.setMaxIterations(MAX_ITERATIONS);
+        bpELU.addListener(getListener("ELU"));
+        neuralNetELU.learn(irisData); 
+        
+        
+        
+        
+        neuralNet = new MultiLayerPerceptron(inputNum, 16, 14, outputNum);
+        bp = neuralNet.getLearningRule();
+        bp.setLearningRate(0.6);
+        bp.setMaxIterations(MAX_ITERATIONS);
+        bp.addListener(getListener("Sigmoid"));
+        neuralNet.learn(irisData);
+        
+        
+    }
+    
+    protected static LearningEventListener getListener(String activationFunction) {
+        return new LearningEventListener() {
+            
+            public String lastMsg; 
+            
             @Override
             public void handleLearningEvent(LearningEvent event) {
                 BackPropagation bp = (BackPropagation) event.getSource();
-                System.out.println("Iteration: "+bp.getCurrentIteration() + " Error: "+bp.getTotalNetworkError());
+                LearningEvent.Type eventName = event.getEventType();
+                if (LearningEvent.Type.LEARNING_STOPPED.equals( eventName )) {
+                    System.out.println(activationFunction + " activation function result - Iteration: "+bp.getCurrentIteration() + " Error: "+bp.getTotalNetworkError());
+                }
             }
-        });
-        
-        neuralNetELU.learn(irisData); 
-        neuralNet.learn(irisData);
-        
-        System.out.println("Done!");
+        };
     }
     
 }
--- src/irisclassification/MultilayerPerceptronELU.java
+++ src/irisclassification/MultilayerPerceptronELU.java
@@ -26,6 +26,7 @@ import org.neuroph.util.NeuralNetworkType;
 import org.neuroph.util.random.RangeRandomizer;
 
 import org.neuroph.core.Neuron;
+import org.neuroph.core.transfer.TransferFunction;
 
 public class MultilayerPerceptronELU extends MultiLayerPerceptron {
     
@@ -33,7 +34,7 @@ public class MultilayerPerceptronELU extends MultiLayerPerceptron {
         super(neuronsInLayers);
     }
     
-    public MultilayerPerceptronELU(ExponentialLinearUnit elu, int... neuronsInLayers) {
+    public MultilayerPerceptronELU(TransferFunction elu, int... neuronsInLayers) {
         super(neuronsInLayers);    
         
         for (int i = 1; i < this.getLayersCount(); i++) {
@@ -41,6 +42,68 @@ public class MultilayerPerceptronELU extends MultiLayerPerceptron {
                 ((Neuron) neuron).setTransferFunction(elu);
             }
         }
+    }
+    
+    /**
+     * Creates MultiLayerPerceptron Network architecture - fully connected
+     * feed forward with specified number of neurons in each layer
+     *
+     * @param neuronsInLayers  collection of neuron numbers in getLayersIterator
+     * @param neuronProperties neuron properties
+     */
+    private void createNetwork(List<Integer> neuronsInLayers, NeuronProperties neuronProperties) {
+
+        // set network type
+        this.setNetworkType(NeuralNetworkType.MULTI_LAYER_PERCEPTRON);
+
+        // create input layer
+        NeuronProperties inputNeuronProperties = new NeuronProperties(InputNeuron.class, Linear.class);
+        Layer layer = LayerFactory.createLayer(neuronsInLayers.get(0), inputNeuronProperties);
+
+        boolean useBias = true; // use bias neurons by default
+        if (neuronProperties.hasProperty("useBias")) {
+            useBias = (Boolean) neuronProperties.getProperty("useBias");
+        }
+
+        if (useBias) {
+            layer.addNeuron(new BiasNeuron());
+        }
+
+        this.addLayer(layer);
+
+        // create layers
+        Layer prevLayer = layer;
+
+        //for(Integer neuronsNum : neuronsInLayers)
+        for (int layerIdx = 1; layerIdx < neuronsInLayers.size(); layerIdx++) {
+            Integer neuronsNum = neuronsInLayers.get(layerIdx);
+            // createLayer layer
+            layer = LayerFactory.createLayer(neuronsNum, neuronProperties);
+
+            if (useBias && (layerIdx < (neuronsInLayers.size() - 1))) {
+                layer.addNeuron(new BiasNeuron());
+            }
+
+            // add created layer to network
+            this.addLayer(layer);
+            // createLayer full connectivity between previous and this layer
+            if (prevLayer != null) {
+                ConnectionFactory.fullConnect(prevLayer, layer);
+            }
+
+            prevLayer = layer;
+        }
+
+        // set input and output cells for network
+        NeuralNetworkFactory.setDefaultIO(this);
+
+        // set learnng rule
+//        this.setLearningRule(new BackPropagation());
+        this.setLearningRule(new MomentumBackpropagation());
+        // this.setLearningRule(new DynamicBackPropagation());
+
+        this.randomizeWeights(new RangeRandomizer(-0.7, 0.7));
+
     }
     
 }
